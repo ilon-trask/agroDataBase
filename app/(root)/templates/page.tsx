10d1b27a9c8727dd47713ec62e9e5b5a26df7a40
ilon-trask <misha007zyt@gmail.com>
2023-12-22 18:08:50 +0200

    fixed bug

--- app/(root)/templates/page.tsx
+++ app/(root)/templates/page.tsx
@@ -6,15 +6,6 @@ import prismadb from "@/lib/prismadb";
 import createServerClient from "@/lib/createServerClient";
 
 async function page() {
-  const supabase = createServerClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  if (!user) throw new Error("немає аккаунту");
-  const prismaUser = await prismadb.user.findFirst({
-    where: { sub: user.id },
-  });
-
   const templates = await prismadb.template.findMany({
     where: { isPublic: true },
   });
